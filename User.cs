cad2cf6795ce82ebaac4d17d440436b9302ad60e
kiruthikaecesnsct <kiruthikar06ecesnsct@gmail.com>
2026-06-27 17:07:27 +0530

    removed roles

--- User.cs
+++ User.cs
@@ -27,16 +27,16 @@ public class User
     public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
-    public string Role { get; set; }
+    //public string Role { get; set; }
     public DateTime DateRegistered { get; set; }
     public bool IsActive { get; set; }
 
-    public User(string username, string password, string email, string role)
+    public User(string username, string password, string email/*, string role*/)
     {
         Username = username;
         Password = password;
         Email = email;
-        Role = role;
+        //Role = role;
         DateRegistered = DateTime.Now;
         IsActive = true;
     }
